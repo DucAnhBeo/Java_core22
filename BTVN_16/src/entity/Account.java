ca8b70d922d073680f7af84cb8bea94113a50cc4
DucEng <112877906+ducanhbeo@users.noreply.github.com>
2023-10-13 14:59:30 +0700

    Add

---
+++ BTVN_16/src/entity/Account.java
@@ -0,0 +1,30 @@
+package entity;
+
+import java.io.Serializable;
+
+public class Account implements Serializable {
+    private static final long serialVersionUID = -6500665823330706018L;
+    private Bank bank;
+    private double balance;
+
+    public Account(Bank bank, double balance) {
+        this.bank = bank;
+        this.balance = balance;
+    }
+
+    public Bank getBank() {
+        return bank;
+    }
+
+    public void setBank(Bank bank) {
+        this.bank = bank;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+}
